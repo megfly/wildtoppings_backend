ab1ac1ca43babdf4c848ba24f8fc3d3c7a6aff91
megfly <mflynn333@gmail.com>
2020-10-26 17:31:03 -0400

    removed instance variables from index

--- app/controllers/api/v1/pizzas_controller.rb
+++ app/controllers/api/v1/pizzas_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::PizzasController < ApplicationController
 
     def index 
-        @pizzas = Pizza.all 
-        render json: @pizzas
+        #we dont need instance variables- because usually we are rendering data in views
+            #get request and returns index of all pizzas in my db in json format
+        pizzas = Pizza.all 
+        render json: pizzas
     end 
 end
