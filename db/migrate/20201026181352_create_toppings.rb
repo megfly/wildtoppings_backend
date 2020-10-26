df9301cd8890d4fed936c80daea9bd5e5aafda99
megfly <mflynn333@gmail.com>
2020-10-26 14:45:06 -0400

    changed the migrations table for createtoppings back to the original way it was written

--- db/migrate/20201026181352_create_toppings.rb
+++ db/migrate/20201026181352_create_toppings.rb
@@ -2,7 +2,7 @@ class CreateToppings < ActiveRecord::Migration[5.2]
   def change
     create_table :toppings do |t|
       t.string :ingredient_name
-      t.belongs_to :pizza
+      t.integer :pizza_id
 
       t.timestamps
     end
