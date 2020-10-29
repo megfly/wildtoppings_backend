1c76b8db21a6c7e1ab5919056d0d4d29b1063280
megfly <mflynn333@gmail.com>
2020-10-29 14:50:38 -0400

    fixed typo

--- app/controllers/api/v1/toppings_controller.rb
+++ app/controllers/api/v1/toppings_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::ToppingsController < ApplicationController
 
     def create
         pizza = Pizza.find(params[:pizza_id])
-        topping = Pizza.Topping.build(topping_params)
+        topping = Pizza.toppings.build(topping_params)
         if topping.save
             render json: topping, status: :accepted
         else 
