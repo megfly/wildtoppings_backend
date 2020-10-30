e010d594908fe85a2d5bc62d4e8184421ee1420d
megfly <mflynn333@gmail.com>
2020-10-30 14:11:30 -0400

    cleaned up toppings controller

--- app/controllers/api/v1/toppings_controller.rb
+++ app/controllers/api/v1/toppings_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::ToppingsController < ApplicationController
-    def index 
-        toppings = Topping.all 
-        render json: toppings, status: 200
-    end 
+    # def index 
+    #     toppings = Topping.all 
+    #     render json: toppings, status: 200
+    # end 
 
-    def show 
-        topping = Topping.find(params[:id])
-        render json: topping, status: 200
-    end 
+    # def show 
+    #     topping = Topping.find(params[:id])
+    #     render json: topping, status: 200
+    # end 
 
     def create
         pizza = Pizza.find(params[:pizza_id])
@@ -20,15 +20,15 @@ class Api::V1::ToppingsController < ApplicationController
         end 
     end 
 
-    def update 
-        topping = Topping.find(params[:id])
-        if topping.update(topping_params)
-            render json: topping, status: :accepted
-        else 
-            render json: {errors: topping.errors.full_messages}, status: 
-            :unprocessible_entity
-        end 
-    end 
+    # def update 
+    #     topping = Topping.find(params[:id])
+    #     if topping.update(topping_params)
+    #         render json: topping, status: :accepted
+    #     else 
+    #         render json: {errors: topping.errors.full_messages}, status: 
+    #         :unprocessible_entity
+    #     end 
+    # end 
 
     def destroy
         topping = Topping.find(params[:id])
