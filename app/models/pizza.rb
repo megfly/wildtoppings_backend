cc92dfb4355270871932abd375b3096aaab14341
megfly <mflynn333@gmail.com>
2020-11-05 10:02:48 -0500

    added validations to models

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,3 +1,5 @@
 class Pizza < ApplicationRecord
     has_many :toppings
+
+    validates :title, :description, presence: true 
 end
--- app/models/topping.rb
+++ app/models/topping.rb
@@ -1,3 +1,5 @@
 class Topping < ApplicationRecord
     belongs_to :pizza
+
+    validates :ingredient_name, presence: true
 end
